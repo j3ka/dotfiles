77e5b7ff004656cc9de3072063fe18731353e24c
j3ka <jeka.web.dev@gmail.com>
2021-03-01 23:57:56 +0200

    i3 config update

---
+++ keyboard_fix.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# setxkbmap -option 'grp:alt_shift_toggle,ctrl:nocaps' -layout us,ru
+setxkbmap -option 'grp:shifts_toggle,ctrl:nocaps' -layout us,ru
---
+++ multi_monitor.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#xrandr --output HDMI-1 --auto --right-of eDP-1
+xrandr --output HDMI-1 --auto --left-of eDP-1
