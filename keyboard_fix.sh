bc3957500bdc873380d62bbc8958255b1821c016
j3ka <j3ka@localhost.localdomain>
2025-07-24 10:51:17 +0200

    Remove configs for emacs, nvim, etc

--- keyboard_fix.sh
+++ keyboard_fix.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# setxkbmap -option 'grp:alt_shift_toggle,ctrl:nocaps' -layout us,ru
-setxkbmap -option 'grp:shifts_toggle,ctrl:nocaps' -layout us,ru
+setxkbmap -option 'grp:alt_shift_toggle,ctrl:nocaps' -layout us,ru
